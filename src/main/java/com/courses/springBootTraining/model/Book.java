e6fc5ec21121801b637258ff8ef781261ae119dc
Ser3q <gornykamil1230@gmail.com>
2022-08-09 18:13:17 +0200

    fix imports

--- src/main/java/com/courses/springBootTraining/model/Book.java
+++ src/main/java/com/courses/springBootTraining/model/Book.java
@@ -1,10 +1,15 @@
 package com.courses.springBootTraining.model;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.ManyToMany;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import java.util.Objects;
 import java.util.Set;
 
